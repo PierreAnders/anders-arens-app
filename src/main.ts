36654d5bfddd39d15ecd8264449b4740ef002e92
guillaume <gbourda@rugbycentric.com>
2022-06-28 13:30:10 +0200

    remove dist folder

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,67 @@
-import {marbellaBlackS, marbellaBlackM, marbellaBlackL, getStock, marbellaBlackSStock, marbellaBlackMStock, marbellaBlackLStock} from "./production/production"
-import {getOrder, guillaumeBourda, guillaumeBourdaOrder, pierreUntas, pierreUntasOrder } from "./front_end/front_end"
-import {getShipping, deliveryTime} from "./shipping/shipping"
+import {
+  marbellaBlackS,
+  marbellaBlackM,
+  marbellaBlackL,
+  getStock,
+  marbellaBlackSStock,
+  marbellaBlackMStock,
+  marbellaBlackLStock,
+} from "./production/production";
+import {
+  getOrder,
+  guillaumeBourda,
+  guillaumeBourdaOrder,
+  pierreUntas,
+  pierreUntasOrder,
+} from "./front_end/front_end";
+import { getShipping, deliveryTime } from "./shipping/shipping";
 
-console.log("\n")
+console.log("\n");
 
-console.log("-------------------------------------------------------------")
-console.log("               WELCOME IN ANDERS ARENS' APP                  ")
-console.log("-------------------------------------------------------------")
+console.log("-------------------------------------------------------------");
+console.log("               WELCOME TO ANDERS ARENS' APP                  ");
+console.log("-------------------------------------------------------------");
 
-console.log("\n")
-console.log("------------------ Here is our catalogue: -------------------")
-console.log("\n")
+console.log("\n");
+console.log("------------------ Here is our catalogue: -------------------");
+console.log("\n");
 
-deliveryTime(marbellaBlackSStock)
-deliveryTime(marbellaBlackMStock)
-deliveryTime(marbellaBlackLStock)
+deliveryTime(marbellaBlackSStock);
+deliveryTime(marbellaBlackMStock);
+deliveryTime(marbellaBlackLStock);
 
-console.log(`${marbellaBlackS.style} ${marbellaBlackS.color} ${marbellaBlackS.size}, €${marbellaBlackS.price}, delivery delay: ${marbellaBlackSStock.timeToShip} days`)
-console.log(`${marbellaBlackM.style} ${marbellaBlackM.color} ${marbellaBlackM.size}, €${marbellaBlackM.price}, delivery delay: ${marbellaBlackMStock.timeToShip} days`)
-console.log(`${marbellaBlackL.style} ${marbellaBlackL.color} ${marbellaBlackL.size}, €${marbellaBlackL.price}, delivery delay: ${marbellaBlackLStock.timeToShip} days`)
+console.log(
+  `${marbellaBlackS.style} ${marbellaBlackS.color} ${marbellaBlackS.size}, €${marbellaBlackS.price}, delivery delay: ${marbellaBlackSStock.timeToShip} days`
+);
+console.log(
+  `${marbellaBlackM.style} ${marbellaBlackM.color} ${marbellaBlackM.size}, €${marbellaBlackM.price}, delivery delay: ${marbellaBlackMStock.timeToShip} days`
+);
+console.log(
+  `${marbellaBlackL.style} ${marbellaBlackL.color} ${marbellaBlackL.size}, €${marbellaBlackL.price}, delivery delay: ${marbellaBlackLStock.timeToShip} days`
+);
 
-console.log("\n")
-console.log("----------------- Here are our orders: ---------------------")
-console.log("\n")
+console.log("\n");
+console.log("----------------- Here are our orders: ---------------------");
+console.log("\n");
 
-getOrder(guillaumeBourdaOrder)
-getOrder(pierreUntasOrder)
+getOrder(guillaumeBourdaOrder);
+getOrder(pierreUntasOrder);
 
-console.log("\n")
-console.log("----------------- Here are our stock: ---------------------")
-console.log("\n")
+console.log("\n");
+console.log("----------------- Here are our stock: ---------------------");
+console.log("\n");
 
-console.log(getStock(marbellaBlackSStock))
-console.log(getStock(marbellaBlackMStock))
-console.log(getStock(marbellaBlackLStock))
+console.log(getStock(marbellaBlackSStock));
+console.log(getStock(marbellaBlackMStock));
+console.log(getStock(marbellaBlackLStock));
 
+console.log("\n");
+console.log("------------- Here are our delivery informations: -----------");
+console.log("\n");
 
-console.log("\n")
-console.log("------------- Here are our delivery informations: -----------")
-console.log("\n")
+getShipping(guillaumeBourdaOrder, guillaumeBourda);
+getShipping(pierreUntasOrder, pierreUntas);
 
-getShipping(guillaumeBourdaOrder, guillaumeBourda)
-getShipping(pierreUntasOrder, pierreUntas)
-
-console.log("\n")
-console.log("-------------------------------------------------------------")
-console.log("\n")
+console.log("\n");
+console.log("-------------------------------------------------------------");
+console.log("\n");
